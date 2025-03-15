711f1107d604aefe8e8f179e4f40bda21ada7e9f
blbergo <bryan.l.bergo@gmail.com>
2025-03-15 10:39:33 -0700

    feat: next setup

--- apps/supabase/functions/health/index.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from 'jsr:@hono/hono'
-
-const functionName = 'health'
-const app = new Hono().basePath(`/${functionName}`)
-
-app.get('/', (c) => c.text('Service is healthy'))
-
-Deno.serve(app.fetch)
