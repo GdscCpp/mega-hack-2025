a048d622b60d025a8f1a15cc4b86536a94224832
Maayan Israel <maayan@Maayans-MacBook-Pro.local>
2025-03-15 15:41:49 -0700

    feat: posts services, create, update, delete

--- services/posts.ts
+++ services/posts.ts
@@ -1,21 +1,90 @@
+"use server";
 import { createClient } from "@/utils/supabase/server"
 
-const createPostAction = async (title: string, content: string, userId: string, /* Add more params as needed*/) => {
-    "use server";
-    // HINT: this may require also creating a new industry and or profession
+const fetchProfessionId = async (profession: string) => {
+    const supabase = createClient();
+
+    console.log('professionsearched:', profession);
+    const { data, error } = await (await supabase)
+        .from("professions")
+        .select("id")
+        .eq("title", profession)
+        .limit(1);
+
+    console.log(data);
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    if (!data || data.length === 0) {
+        return null;
+    }
+
+    return data[0].id;
 }
 
-const getPaginatePosts = async (page: number, limit: number) => {
+export const createPostAction = async (title: string, content: string, company: string, createdAt: string, profession: string, industry: string, userId?: string) => {
+    const supabase = createClient();
 
+    const professionId = await fetchProfessionId(profession);
+
+    const { data, error } = await (await supabase)
+        .from('posts')
+        .insert({ company, industry, title, content, user_id: userId, profession_id: professionId, created_at: createdAt })
+        .select();
+
+    console.log(data);
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return data;
+}
+
+export const updatePostAction = async (id: number, updatedValues: { title?: string, content?: string, company?: string, industry?: string }) => {
+    const supabase = createClient();
+
+    const { data, error } = await (await supabase)
+        .from('posts')
+        .update(updatedValues)
+        .eq('id', id)
+        .select();
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return data;
 }
 
 export const deletePostAction = async (id: number) => {
-    "use server";
+    const supabase = createClient();
 
+    const { error } = await (await supabase)
+        .from('posts')
+        .delete()
+        .eq('id', id);
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return true;
 }
 
-export const postService = {
-    createPostAction,
-    getPaginatePosts,
-    deletePostAction
+export const getPaginatedPosts = async (page: number, limit: number) => {
+    const supabase = createClient();
+
+    const { data, error } = await (await supabase)
+        .from('posts')
+        .select('*')
+        .range(page, limit);
+
+    if (error) {
+        throw new Error(error.message);
+    }
+
+    return data;
 }
